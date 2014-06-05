0f30bed3d4c24ca4864da987a418990ddfa8551b
David T. Sadler <davidtsadler@googlemail.com>
2014-06-05 13:05:58 +0100

    Change notation of property names.
    Property names will now follow the eBay notation in the XML.

--- src/DTS/eBaySDK/BusinessPoliciesManagement/Types/ConsolidationJob.php
+++ src/DTS/eBaySDK/BusinessPoliciesManagement/Types/ConsolidationJob.php
@@ -19,10 +19,10 @@ namespace DTS\eBaySDK\BusinessPoliciesManagement\Types;
 
 /**
  *
- * @property integer $jobId
- * @property DTS\eBaySDK\BusinessPoliciesManagement\Enums\ConsolidationJobStatus(string) $jobStatus
- * @property DTS\eBaySDK\BusinessPoliciesManagement\Enums\ConsolidationJobType(string) $jobType
- * @property integer $siteId
+ * @property integer $JobId
+ * @property DTS\eBaySDK\BusinessPoliciesManagement\Enums\ConsolidationJobStatus(string) $JobStatus
+ * @property DTS\eBaySDK\BusinessPoliciesManagement\Enums\ConsolidationJobType(string) $JobType
+ * @property integer $SiteId
  */
 class ConsolidationJob extends \DTS\eBaySDK\Types\BaseType
 {
@@ -30,25 +30,25 @@ class ConsolidationJob extends \DTS\eBaySDK\Types\BaseType
      * @var array Properties belonging to objects of this class.
      */
     private static $propertyTypes = array(
-        'jobId' => array(
+        'JobId' => array(
             'type' => 'integer',
             'unbound' => false,
             'attribute' => false,
             'elementName' => 'JobId'
         ),
-        'jobStatus' => array(
+        'JobStatus' => array(
             'type' => 'string',
             'unbound' => false,
             'attribute' => false,
             'elementName' => 'JobStatus'
         ),
-        'jobType' => array(
+        'JobType' => array(
             'type' => 'string',
             'unbound' => false,
             'attribute' => false,
             'elementName' => 'JobType'
         ),
-        'siteId' => array(
+        'SiteId' => array(
             'type' => 'integer',
             'unbound' => false,
             'attribute' => false,
@@ -61,18 +61,12 @@ class ConsolidationJob extends \DTS\eBaySDK\Types\BaseType
      */
     public function __construct(array $values = array())
     {
-        $elementNamesMap = self::buildElementNamesMap(self::$propertyTypes);
-
-        list($parentValues, $childValues) = self::getParentValues($elementNamesMap, self::$propertyTypes, $values);
+        list($parentValues, $childValues) = self::getParentValues(self::$propertyTypes, $values);
 
         parent::__construct($parentValues);
 
         if (!array_key_exists(__CLASS__, self::$properties)) {
             self::$properties[__CLASS__] = array_merge(self::$properties[get_parent_class()], self::$propertyTypes);
-        }
-
-        if (!array_key_exists(__CLASS__, self::$elementNames)) {
-            self::$elementNames[__CLASS__] = array_merge(self::$elementNames[get_parent_class()], $elementNamesMap);
         }
 
         if (!array_key_exists(__CLASS__, self::$xmlNamespaces)) {
--- src/DTS/eBaySDK/BusinessPoliciesManagement/Types/ShippingPolicyInfo.php
+++ src/DTS/eBaySDK/BusinessPoliciesManagement/Types/ShippingPolicyInfo.php
@@ -19,6 +19,7 @@ namespace DTS\eBaySDK\BusinessPoliciesManagement\Types;
 
 /**
  *
+ * @property boolean $GlobalShipping
  * @property integer $dispatchTimeMax
  * @property string $dispatchTimeReason
  * @property string $domesticRateTable
@@ -26,7 +27,6 @@ namespace DTS\eBaySDK\BusinessPoliciesManagement\Types;
  * @property string $domesticShippingType
  * @property string $excludeShipToLocation
  * @property DTS\eBaySDK\BusinessPoliciesManagement\Types\FreightShipping $freightShipping
- * @property boolean $globalShipping
  * @property DTS\eBaySDK\BusinessPoliciesManagement\Types\Insurance $insurance
  * @property DTS\eBaySDK\BusinessPoliciesManagement\Types\Amount $internationalPackagingHandlingCosts
  * @property string $intlRateTable
@@ -45,6 +45,12 @@ class ShippingPolicyInfo extends \DTS\eBaySDK\Types\BaseType
      * @var array Properties belonging to objects of this class.
      */
     private static $propertyTypes = array(
+        'GlobalShipping' => array(
+            'type' => 'boolean',
+            'unbound' => false,
+            'attribute' => false,
+            'elementName' => 'GlobalShipping'
+        ),
         'dispatchTimeMax' => array(
             'type' => 'integer',
             'unbound' => false,
@@ -86,12 +92,6 @@ class ShippingPolicyInfo extends \DTS\eBaySDK\Types\BaseType
             'unbound' => false,
             'attribute' => false,
             'elementName' => 'freightShipping'
-        ),
-        'globalShipping' => array(
-            'type' => 'boolean',
-            'unbound' => false,
-            'attribute' => false,
-            'elementName' => 'GlobalShipping'
         ),
         'insurance' => array(
             'type' => 'DTS\eBaySDK\BusinessPoliciesManagement\Types\Insurance',
@@ -166,18 +166,12 @@ class ShippingPolicyInfo extends \DTS\eBaySDK\Types\BaseType
      */
     public function __construct(array $values = array())
     {
-        $elementNamesMap = self::buildElementNamesMap(self::$propertyTypes);
-
-        list($parentValues, $childValues) = self::getParentValues($elementNamesMap, self::$propertyTypes, $values);
+        list($parentValues, $childValues) = self::getParentValues(self::$propertyTypes, $values);
 
         parent::__construct($parentValues);
 
         if (!array_key_exists(__CLASS__, self::$properties)) {
             self::$properties[__CLASS__] = array_merge(self::$properties[get_parent_class()], self::$propertyTypes);
-        }
-
-        if (!array_key_exists(__CLASS__, self::$elementNames)) {
-            self::$elementNames[__CLASS__] = array_merge(self::$elementNames[get_parent_class()], $elementNamesMap);
         }
 
         if (!array_key_exists(__CLASS__, self::$xmlNamespaces)) {
