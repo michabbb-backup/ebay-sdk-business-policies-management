5b7bf835c61173481a083fb4080019fb70d5946c
David T. Sadler <davidtsadler@googlemail.com>
2014-07-05 11:51:09 +0100

    refactor: pass request object to callOperation method
    The method parameters of BaseService::callOperation has changed so that
    a request object must be passed is as the second parameter.
    
    [27bff98](https://github.com/davidtsadler/ebay-sdk/commit/27bff98383695381e6872430ae91b495be9a9b4d)

--- test/DTS/eBaySDK/BusinessPoliciesManagement/Mocks/Service.php
+++ test/DTS/eBaySDK/BusinessPoliciesManagement/Mocks/Service.php
@@ -1,6 +1,8 @@
 <?php
 namespace DTS\eBaySDK\BusinessPoliciesManagement\Mocks;
 
+use DTS\eBaySDK\Mocks\ComplexClass;
+
 class Service extends \DTS\eBaySDK\BusinessPoliciesManagement\Services\BusinessPoliciesManagementBaseService
 {
     public function __construct($config = array(), \DTS\eBaySDK\Interfaces\HttpClientInterface $httpClient = null)
@@ -12,7 +14,7 @@ class Service extends \DTS\eBaySDK\BusinessPoliciesManagement\Services\BusinessP
     {
         return $this->callOperation(
             'testOperation',
-            '',
+            new ComplexClass(),
             '\DTS\eBaySDK\Mocks\ComplexClass'
         );
     }
